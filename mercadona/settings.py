059c0b42c95960e2c83ac6186b42b020fa9e3d43
Krima <krima95@gmail.com>
2023-10-15 23:45:18 +0200

    Update settings.py

--- mercadona/settings.py
+++ mercadona/settings.py
@@ -79,6 +79,7 @@ WSGI_APPLICATION = "mercadona.wsgi.application"
 # Render postgresQL Database
 
 DATABASES = {
+    """ 'default': dj_database_url.config(default=config('DATABASE_URL')), """
     
     'default': {
             'ENGINE': 'django.db.backends.postgresql',
