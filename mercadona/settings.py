1b968a61b8edc0e1ed3167fc16e90644e2a00943
krima95 <krima95@gmail.com>
2023-10-15 22:04:44 +0200

    env a la racine

--- mercadona/settings.py
+++ mercadona/settings.py
@@ -2,10 +2,7 @@ import os
 from pathlib import Path
 import dj_database_url
 from decouple import config, Csv
-"""
-env = environ.Env()
-environ.Env.read_env()
-"""
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
