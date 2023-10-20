1e755aef06ab8e2bf11e59c075f67adaa1352c7a
krima95 <krima95@gmail.com>
2023-10-20 22:08:54 +0200

    whitenoise

--- mercadona/settings.py
+++ mercadona/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
     "crispy_forms",
     "rest_framework",
     'dj_database_url',
-    #"whitenoise.runserver_nostatic",
+    "whitenoise.runserver_nostatic",
 
 
 ]
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
-    #"whitenoise.middleware.WhiteNoiseMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "defender.middleware.FailedLoginMiddleware",
-    #"django.contrib.staticfiles.middleware.StaticFilesMiddleware",
+    "django.contrib.staticfiles.middleware.StaticFilesMiddleware",
 
 
 
