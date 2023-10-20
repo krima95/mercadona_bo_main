f2833d794f3d6510b8b847738fdb2be495fa0c0f
krima95 <krima95@gmail.com>
2023-10-20 23:34:07 +0200

    middleware comment

--- mercadona/settings.py
+++ mercadona/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "defender.middleware.FailedLoginMiddleware",
-    "django.contrib.staticfiles.middleware.StaticFilesMiddleware",
+    #"django.contrib.staticfiles.middleware.StaticFilesMiddleware",
 
 
 
